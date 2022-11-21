60d1974730aced3818e0d276d27e7dd09e5d4084
Scrooge McDuck <angelisium@daum.net>
2022-11-21 16:22:56 +0100

    Update index.js

---
+++ source/index.js
@@ -0,0 +1,17 @@
+const Enum = require('./type/enum.js');
+const List = require('./type/list.js');
+const Bytes = require('./type/bytes.js');
+const Class = require('./type/class.js');
+const IntMap = require('./type/map/int.js');
+const StringMap = require('./type/map/string.js');
+const ObjectMap = require('./type/map/object.js');
+
+module.exports = {
+	Enum,
+	List,
+	Bytes,
+	Class,
+	IntMap,
+	StringMap,
+	ObjectMap
+};
