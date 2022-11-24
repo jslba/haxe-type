b2fda4c8468b0ba1626688f40379ad4d3c0d0d1c
Scrooge McDuck <angelisium@daum.net>
2022-11-24 11:01:46 +0100

    Create index.d.ts

---
+++ type/index.d.ts
@@ -0,0 +1,8 @@
+import Enum = require("./type/enum.js");
+import List = require("./type/list.js");
+import Bytes = require("./type/bytes.js");
+import Class = require("./type/class.js");
+import IntMap = require("./type/map/int.js");
+import StringMap = require("./type/map/string.js");
+
+export { Enum, List, Bytes, Class, IntMap, StringMap };
